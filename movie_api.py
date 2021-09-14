68883daa37f690e7893a5bdcefd2eb7f2ac96304
Sleeping-Suit <hrcho721@gmail.com>
2021-09-14 17:18:03 +0900

    현재온도 표기 진행

--- movie_api.py
+++ movie_api.py
@@ -1,10 +1,21 @@
+from requests.models import Response
 from movie_model import MovieModel
+from temp_model import TempModel
 import requests
+from requests.api import request
+from bs4 import BeautifulSoup
+
+def callTempApi():
+    url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%ED%98%84%EC%9E%AC%EC%98%A8%EB%8F%84'
+    
+    req = requests.get(url)
+    
+    soup = BeautifulSoup(req.content, 'html.parser')
+    temp = soup.select('span.todaytemp')[0].text
+    return temp
 
 def callMovieApi(page=1):
-    url = f'''
-        https://yts.mx/api/v2/list_movies.json?sort_by=rating&page_number=1&limit=20
-    '''
+    url = f'''https://yts.mx/api/v2/list_movies.json?sort_by=rating&page_number=1&limit=20'''
 
     response = requests.get(url)
 
--- router.py
+++ router.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", movies=ma.callMovieApi())
+    return render_template("index.html", movies = ma.callMovieApi(), temps = ma.callTempApi()) 
 
 if __name__ == "__main__":
     app.run(debug=True)
