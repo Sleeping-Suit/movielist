bc2073ae93542cd212713908f907a301bd025d6b
Sleeping-Suit <hrcho721@gmail.com>
2021-09-14 15:25:10 +0900

    영화 리스트 표시

---
+++ movie_api.py
@@ -0,0 +1,23 @@
+from movie_model import MovieModel
+import requests
+
+def callMovieApi(page=1):
+    url = f'''
+        https://yts.mx/api/v2/list_movies.json?sort_by=rating&page_number=1&limit=20
+    '''
+
+    response = requests.get(url)
+
+    responseDict = response.json()
+    movies = responseDict["data"]["movies"]
+    return convert_model(movies)
+
+
+def convert_model(movies):
+    list = []
+
+    for movie in movies:
+        movie_model = MovieModel(movie["small_cover_image"], movie["title"], movie["rating"], movie["summary"])
+        list.append(movie_model)
+
+    return list
---
+++ movie_model.py
@@ -0,0 +1,7 @@
+
+class MovieModel:
+    def __init__(self, small_cover_image, title, rating, summary):
+        self.small_cover_image = small_cover_image
+        self.title = title
+        self.rating = rating
+        self.summary = summary
